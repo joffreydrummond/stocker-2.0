7d3fcf3656e965549bb29253a8e4ce7ddb33f46e
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2019-07-09 19:34:02 -0700

    added client folder back with config and models folder

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+      clientID:
+        "845894195134-mifdn30c47i8fe1vcdmi598nivh7s8ga.apps.googleusercontent.com",
+      clientSecret: "VRN_dWR3J3uzV7LHoR2jN8bZ"
+    },
+    facebook: {
+        appID:
+          "845894195134-mifdn30c47i8fe1vcdmi598nivh7s8ga.apps.googleusercontent.com",
+        appSecret: "56a32dadceaa5a18221b18d704cc3519"
+      }
+  };
