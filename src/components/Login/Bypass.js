300edbdb5209119b948363eb76b5904c90b3fff0
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2019-07-23 20:49:42 -0700

    added google login

--- src/components/Login/Bypass.js
+++ src/components/Login/Bypass.js
@@ -1,10 +1,12 @@
 import React from "react";
+import LoginBtn from "./Navbar";
 
 function Bypass () {
   return (
     <div className="row">
-    <div className="col-5" />
-    <a className="col-2 loginButton" href="/home/">Login</a>
+      <div className="col-5" />
+      <LoginBtn/>
+    {/* <a className="col-2 loginButton" href="/home/">Login</a> */}
     <div className="col-5" />
     </div>
   );
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./style.css";
-import Bypass from "./Bypass";
+// import Bypass from "./Bypass";
+import LoginBtn from "./Navbar";
 
 function Login () {
   return (
-    <Bypass />
+    <div className="row">
+      <div className="col-5" />
+      <h2>Click to login with Google</h2>
+    <LoginBtn/>
+  {/* <a className="col-2 loginButton" href="/home/">Login</a> */}
+  <div className="col-5" />
+  </div>
   );
 };
 
